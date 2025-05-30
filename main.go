1701aa3a17088ef7c502c264c332eb1ef920e5a7
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-30 21:27:26 +0700

    Form Upload File

--- main.go
+++ main.go
@@ -3,55 +3,79 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
-	"path"
+	"os"
+	"path/filepath"
 )
-
-func routeIndexGet(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "GET" {
-        var filePath = path.Join("views", "index.html")
-        var tmpl, errTemplate = template.New("form").ParseFiles(filePath)
-        if errTemplate != nil {
-            http.Error(w, errTemplate.Error(), http.StatusInternalServerError)
-            return
-        }
-        var err = tmpl.Execute(w, nil)
-
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        return
-    }
-
-    http.Error(w, "", http.StatusBadRequest)
-}
-func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        var filePath = path.Join("views", "index.html")
-        var tmpl = template.Must(template.New("result").ParseFiles(filePath))
-
-        if err := r.ParseForm(); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-
-        var name = r.FormValue("name")
-        var message = r.Form.Get("message")
-
-        var data = map[string]string{"name": name, "message": message}
-
-        if err := tmpl.Execute(w, data); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        return
-    }
-
-    http.Error(w, "", http.StatusBadRequest)
-}
 
 func main() {
     http.HandleFunc("/", routeIndexGet)
     http.HandleFunc("/process", routeSubmitPost)
+
     fmt.Println("server started at localhost:9000")
     http.ListenAndServe(":9000", nil)
+}
+
+func routeIndexGet(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "GET" {
+        http.Error(w, "", http.StatusBadRequest)
+        return
+    }
+
+    var tmpl = template.Must(template.ParseFiles("view.html"))
+    var err = tmpl.Execute(w, nil)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        http.Error(w, "", http.StatusBadRequest)
+        return
+    }
+
+    if err := r.ParseMultipartForm(1024); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    alias := r.FormValue("alias")
+
+    uploadedFile, handler, err := r.FormFile("file")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer uploadedFile.Close()
+
+    dir, err := os.Getwd()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    filename := handler.Filename
+    if alias != "" {
+        filename = fmt.Sprintf("%s%s", alias, filepath.Ext(handler.Filename))
+    }
+
+    fileLocation := filepath.Join(dir, "files", filename)
+    targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer targetFile.Close()
+
+    if _, err := io.Copy(targetFile, uploadedFile); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Write([]byte("done"))
+
+    // ...
 }
