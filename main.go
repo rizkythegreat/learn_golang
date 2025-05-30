35022328292c4762a1eb83ac31697b6ab7a499cd
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-30 20:47:00 +0700

    Template: Actions & Variables

--- main.go
+++ main.go
@@ -4,38 +4,43 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path"
 )
 
-type M map[string]interface{}
+type Info struct {
+    Affiliation string
+    Address string
+}
+
+type Person struct {
+    Name string
+    Gender string
+    Hobbies []string
+    Info Info
+}
+
+func (t Info) GetAffiliationDetailInfo() string {
+    return "have 31 divisions"
+}
 
 func main() {
-    http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-     var data = M{"name": "Batman"}
-     var tmpl = template.Must(template.ParseFiles(
-         "views/index.html",
-         "views/_header.html",
-         "views/_message.html",
-     ))
-
-     var err = tmpl.ExecuteTemplate(w, "index", data)
-     if err != nil {
-         http.Error(w, err.Error(), http.StatusInternalServerError)
-     }
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        var person = Person{
+            Name:    "Bruce Wayne",
+            Gender:  "male",
+            Hobbies: []string{"Reading Books", "Traveling", "Buying things"},
+            Info:    Info{"Wayne Enterprises", "Gotham City"},
+        }
+        var filePath = path.Join("views", "index.html")
+        var tmpl, errTemplate = template.ParseFiles(filePath)
+        if errTemplate != nil {
+            http.Error(w, errTemplate.Error(), http.StatusInternalServerError)
+            return
+        }
+        if err := tmpl.Execute(w, person); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
     })
-
-    http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-     var data = M{"name": "Batman"}
-     var tmpl = template.Must(template.ParseFiles(
-         "views/about.html",
-         "views/_header.html",
-         "views/_message.html",
-     ))
-
-     var err = tmpl.ExecuteTemplate(w, "about", data)
-     if err != nil {
-         http.Error(w, err.Error(), http.StatusInternalServerError)
-     }
- })
     fmt.Println("server started at localhost:9000")
     http.ListenAndServe(":9000", nil)
 }
